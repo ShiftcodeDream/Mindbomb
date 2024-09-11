74b87df29ee59eeeb8bc493bd05cee3a058918d0
Matthias <shiftcode@gmail.com>
2024-09-11 23:47:43 +0200

    First gradient

--- media/ParaScroll.js
+++ media/ParaScroll.js
@@ -11,14 +11,37 @@ class ParaScroll {
   // Loads resources and returns a Promise
   // You can make long precalculations here.
   load() {
-    return this.demoManager.loadResource('mind-logo1.png').then(introImage => {
-      this.introImage = introImage;
+    return Promise.all(this.demoManager.loadResource(['Red_LB.png', 'Red_Bubles.png', 'Carre_LB.png', 'Dalton_20x20.png'])).then(data => {
+      [this.sprites, this.bubles, this.backLogo, this.font] = data;
     });
   }
 
   // Initialize the demo (all resources are already loaded)
   // Initialize scrolltexts here (for example)
   init() {
+    let i, col;
+    this.back = new canvas(64, 640); // TODO : adjust height
+    const ct = this.back.contex;
+    ct.drawImage(this.backLogo.img,0,0);
+    const source = ct.getImageData(0,0,64,64);
+    const cible = ct.getImageData(0,0,64,64);
+    "033-055-067 033-055-077 033-055-076"
+    .split(' ').forEach((palette, numPal) => {
+      const pal = palette.split('-').map(coul => coul.split('').map(k=>k*32));
+      for(i=0; i<source.data.length; i+=4){
+        if(source.data[i+2] > 0){
+          switch(source.data[i+2]){
+            case 0x60: col=0; break;
+            case 0xA0: col=1; break;
+            case 0xE0: col=2; break;
+          }
+          cible.data[i] = pal[col][0];
+          cible.data[i+1] = pal[col][1];
+          cible.data[i+2] = pal[col][2];
+        }
+      }
+      ct.putImageData(cible, 0, numPal*64);
+    });
     this.running = true;
   }
 
@@ -30,7 +53,6 @@ class ParaScroll {
       this.can = new canvas(640, 400, "main");
       this.ctx = this.can.contex;
       document.body.addEventListener('keydown', this.onKeyPressed);
-      setTimeout(this.stop, 4500);
       window.requestAnimFrame(this.main);
     });
   }
@@ -38,11 +60,7 @@ class ParaScroll {
   // Main loop, called by Codef requestAnimFrame
   main() {
     if (this.running) {
-      this.introImage.draw(this.can, 16, 16);
-      this.ctx.font = 'bold 24px "Comic sans MS", serif';
-      this.ctx.fillStyle = '#F80';
-      this.ctx.textAlign = 'center';
-      this.ctx.fillText("This will be the " + this.constructor.name + " demo.", 320, 200);
+      this.back.draw(this.can,0,0);
       window.requestAnimFrame(this.main);
     } else {
       this.end();
